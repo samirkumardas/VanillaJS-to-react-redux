d189d0bc47ae3b38528c8b1c3c53c26aea99990d
Samir Das <cse.samir@gmail.com>
2017-04-01 00:32:10 +0600

    inital projects

---
+++ vanilla/webpack.config.js
@@ -0,0 +1,18 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
+const webpack = require('webpack'); //to access built-in plugins
+const path = require('path');
+
+const config = {
+  entry: './index.js',
+  output: {
+    filename: 'app.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({template: './index.html'})
+  ],
+  devServer: {
+    port: 9900
+  }
+};
+
+module.exports = config;
